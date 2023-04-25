20aaeab200fb3fae10265fdf717ab5ef885e5cf7
Uzaeir <paf_uzair@yahoo.com>
2023-04-25 14:55:46 +0200

    chore: update visit with some methods

--- cypress/support/pages/visit.js
+++ cypress/support/pages/visit.js
@@ -13,6 +13,23 @@ class visit {
     )
     assert.deepEqual({}, {})
   }
+
+  page_assert () {
+    cy.get('#search_form_input_homepage').should(
+      'have.attr',
+      'placeholder',
+      'Search the web without being tracked'
+    )
+    assert.deepEqual({}, {})
+  }
+  page_get_title () {
+    let array_list = []
+    //cy.log('Starts')
+    cy.get('li.badge-link__bullet  > span').each($ele => {
+      array_list.push($ele.text().trim())
+    })
+    return array_list
+  }
 }
 
 export default visit
